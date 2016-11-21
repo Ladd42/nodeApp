94eed06883a05b6a05adbaa2b11b0d1a35d93f88
Alexander <xander19@btinternet.com>
2016-11-21 15:42:47 +0000

    part 2

---
+++ connect.js
@@ -0,0 +1,7 @@
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+//and our HTTP server
+var http = require('http');
+//setup our port
+var port = process.env.PORT || 1337;
+
